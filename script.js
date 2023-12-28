e3c3df160d7bce48b8bf5b7c201f0df1f98f3dd4
fatih <alfatihrizqialfian@gmail.com>
2023-12-28 14:46:36 +0700

    finishing project

--- script.js
+++ script.js
@@ -8,16 +8,38 @@ window.addEventListener('scroll', () => {
   }
 });
 
-let currentScrollPosition = 0;
-let scrollAmount = 300;
+var cards = document.querySelectorAll('.card');
+var nextButton = document.querySelector('.next-button');
+var prevButton = document.querySelector('.prev-button');
+var currentIndex = 0;
 
-const productC = document.querySelector('.product-card-container');
-const scrollP = document.querySelector('.product-scroll');
-
-let maxScroll = -productC.offsetWidth + scrollP.offsetWidth;
-
-function scrollHorizontally(val){
-    currentScrollPosition += (val + scrollAmount);
-
-    productC.style.left = currentScrollPosition + "px" ;
+function updateButtons() {
+  var cardsPerView = getCardsPerView();
+  prevButton.style.visibility = currentIndex > 0 ? 'visible' : 'hidden';
+  nextButton.style.visibility =
+    currentIndex < cards.length - cardsPerView ? 'visible' : 'hidden';
 }
+
+function getCardsPerView() {
+  var cardWidth = cards[0].offsetWidth;
+  var viewportWidth = window.innerWidth;
+  return Math.floor(viewportWidth / cardWidth);
+}
+
+nextButton.addEventListener('click', function () {
+  var cardsPerView = getCardsPerView();
+  if (currentIndex + cardsPerView < cards.length) {
+    currentIndex += cardsPerView;
+    cards[currentIndex].scrollIntoView({ behavior: 'smooth' });
+  }
+  updateButtons();
+});
+
+prevButton.addEventListener('click', function () {
+  var cardsPerView = getCardsPerView();
+  if (currentIndex - cardsPerView >= 0) {
+    currentIndex -= cardsPerView;
+    cards[currentIndex].scrollIntoView({ behavior: 'smooth' });
+  }
+  updateButtons();
+});
